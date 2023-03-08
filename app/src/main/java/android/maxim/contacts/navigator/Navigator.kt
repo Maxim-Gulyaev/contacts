3b170965040c20d850ef09f8ca0e8dc282b345d6
MaximGuljaev <maximuspo4ta@gmail.com>
2023-03-08 11:54:52 +0300

    created navigator

---
+++ app/src/main/java/android/maxim/contacts/navigator/Navigator.kt
@@ -0,0 +1,25 @@
+package android.maxim.contacts.navigator
+
+interface Navigator {
+
+    fun showContactDetails()
+
+    fun addContact()
+
+    fun cancel()
+
+    fun saveNewContact()
+
+    fun backToContactList()
+
+    fun editContact()
+
+    fun deleteContact()
+
+    fun call()
+
+    fun sendSms()
+
+    fun saveEditedContact()
+
+}
