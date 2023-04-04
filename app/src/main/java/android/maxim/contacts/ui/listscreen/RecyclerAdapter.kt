9769c94bb852d061bc1867c02d8ce05870d2a5b3
MaximGuljaev <maximuspo4ta@gmail.com>
2023-04-04 11:04:14 +0300

    added recycler adapter

---
+++ app/src/main/java/android/maxim/contacts/ui/listscreen/RecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package android.maxim.contacts.ui.listscreen
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class ListRecyclerAdapter: RecyclerView.Adapter<ListRecyclerAdapter.ListViewHolder>() {
+
+    class ListViewHolder(itemView: View): ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
