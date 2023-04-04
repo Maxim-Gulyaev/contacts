8aede519dd877b3201c26232cf64c07c85b44486
MaximGuljaev <maximuspo4ta@gmail.com>
2023-04-04 22:54:18 +0300

    added injectListViewModel in ViewModelComponent

--- app/src/main/java/android/maxim/contacts/di/component/ViewModelComponent.kt
+++ app/src/main/java/android/maxim/contacts/di/component/ViewModelComponent.kt
@@ -3,11 +3,12 @@ package android.maxim.contacts.di.component
 
 import android.maxim.contacts.di.module.ViewModelModule
 import android.maxim.contacts.ui.addscreen.AddViewModel
+import android.maxim.contacts.ui.listscreen.ListViewModel
 import dagger.Subcomponent
 
 @Subcomponent(modules = [ViewModelModule::class])
 interface ViewModelComponent {
 
     fun injectAddViewModel(addViewModel: AddViewModel)
-
+    fun injectListViewModel(listViewModel: ListViewModel)
 }
