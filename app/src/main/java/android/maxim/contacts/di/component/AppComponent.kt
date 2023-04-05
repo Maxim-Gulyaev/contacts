cfaf252c9c776123e947029620b3d6f9e68b7774
MaximGuljaev <maximuspo4ta@gmail.com>
2023-04-05 21:45:53 +0300

    added ListViewModelFactory im component and module

--- app/src/main/java/android/maxim/contacts/di/component/AppComponent.kt
+++ app/src/main/java/android/maxim/contacts/di/component/AppComponent.kt
@@ -3,12 +3,15 @@ package android.maxim.contacts.di.component
 import android.maxim.contacts.di.module.AppModule
 import android.maxim.contacts.di.module.DataModule
 import android.maxim.contacts.ui.addscreen.AddFragment
+import android.maxim.contacts.ui.listscreen.ListFragment
 import dagger.Component
 
 @Component(modules = [DataModule::class, AppModule::class])
 interface AppComponent {
 
     fun injectAddFragment(addFragment: AddFragment)
+
+    fun injectListFragment(listFragment: ListFragment)
 
     fun viewModelComponent(): ViewModelComponent
 }
--- app/src/main/java/android/maxim/contacts/di/module/AppModule.kt
+++ app/src/main/java/android/maxim/contacts/di/module/AppModule.kt
@@ -3,15 +3,23 @@ package android.maxim.contacts.di.module
 import android.app.Application
 import android.maxim.contacts.model.repository.Repository
 import android.maxim.contacts.ui.addscreen.AddViewModelFactory
+import android.maxim.contacts.ui.listscreen.ListViewModelFactory
 import dagger.Module
 import dagger.Provides
 
 @Module
 class AppModule {
-
+    //
     @Provides
     fun provideAddViewModelFactory(repository: Repository): AddViewModelFactory {
         return AddViewModelFactory(
+            repository = repository
+        )
+    }
+
+    @Provides
+    fun provideListViewModelFactory(repository: Repository): ListViewModelFactory {
+        return ListViewModelFactory(
             repository = repository
         )
     }
